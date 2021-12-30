657fc8c0d1c25cabb7c9701bfedfe4ee6f264982
Shawn Stern <sstern@twilio.com>
2021-12-30 15:17:32 -0800

    feat: load corgi upboops from Hasura DB via mutation w/ conflict logic

--- functions/load-corgis.js
+++ functions/load-corgis.js
@@ -1,4 +1,5 @@
 const fetch = require('node-fetch');
+const { hasuraRequest } = require('./util/hasura');
 
 exports.handler = async () => {
   const corgis = await fetch('https://no-cors-api.netlify.app/api/corgis').then(
@@ -14,16 +15,39 @@ exports.handler = async () => {
     }
   ).then((res) => res.json());
 
-  const [unsplash] = await Promise.all([unsplashPromise]);
+  const hasuraPromise = hasuraRequest({
+    query: `
+      mutation InsertOrUpdateBoops($corgis: [boops_insert_input!]!) {
+        boops: insert_boops(objects: $corgis, on_conflict: {constraint: boops_pkey, update_columns: id}) {
+          returning {
+            id
+            count
+          }
+        }
+      }
+    `,
+    variables: {
+      // Insert any new corgis to Hasura DB w/ count of 0. If they already exist (conflict)
+      // then the actual value in the DB will be returned instead of 0 :)
+      corgis: corgis.map(({ id }) => ({ id, count: 0 })),
+    },
+  });
+
+  const [unsplash, hasuraData] = await Promise.all([
+    unsplashPromise,
+    hasuraPromise,
+  ]);
 
   const completeData = corgis.map((corgi) => {
-    const photo = unsplash.find((p) => corgi.id === p.id);
+    const photo = unsplash.find((p) => p.id === corgi.id);
+    const boops = hasuraData.boops.returning.find((b) => b.id === corgi.id);
 
     return {
       ...corgi,
       alt: photo.alt_description,
       credit: photo.user.name,
       url: `${photo.urls.raw}&auto=format&fit=crop&w=300&h=300&quality=80&crop=entropy`,
+      boops: boops.count,
     };
   });
 
